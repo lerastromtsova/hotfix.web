5b85d63d99dc417606c5552aab97e84e06f73da1
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-09-09 23:31:22 +0300

    #1: Added price check on order

--- src/panels/Place.js
+++ src/panels/Place.js
@@ -101,9 +101,11 @@ const Place = ({ item, order, onIncrementPosition, onDecrementPosition, area }) 
         )))}
       </ul>
       <footer className="Place__footer">
-        <Link to={`/basket/${area.id}/${item.id}`} className="Place__order">
-          Оформить заказ ({price})
-        </Link>
+        {price !== '0' &&
+          <Link to={`/basket/${area.id}/${item.id}`} className="Place__order">
+            Оформить заказ ({price})
+          </Link>
+        }
       </footer>
     </div>
   );
